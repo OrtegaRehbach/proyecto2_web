1d0dfe0930f68710cd23278fc4d7dd25b19249bb
Alejandro Ortega <ort18248@uvg.edu.gt>
2023-05-25 18:39:33 -0600

    Search component Estructura y Estilo

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent {
-
+  searchedGames =  [
+    {name: "Name 1"},
+    {name: "Name 2"},
+    {name: "Name 3"},
+    {name: "Name 4"},
+    {name: "Name 5"},
+    {name: "Name 6"},
+    {name: "Name 7"},
+    {name: "Name 8"},
+    {name: "Name 9"},
+    {name: "Name 10"},
+    {name: "Name 11"},
+    {name: "Name 12"}
+  ]
 }
